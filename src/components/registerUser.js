284544ed0b9b84e4e874e41a2c773e438f46bbef
Enio Javier <javiernieto1989@gmail.com>
2020-09-26 17:27:04 -0400

    Create validations with Joi library

--- src/components/registerUser.js
+++ src/components/registerUser.js
@@ -1,6 +1,7 @@
 import React, { Component } from "react";
 import axios from "axios";
 import Input from "./input";
+import Joi from "joi";
 
 class RegisterUser extends Component {
   state = {
@@ -14,15 +15,37 @@ class RegisterUser extends Component {
     errors: {},
   };
 
+  schema = Joi.object({
+    name: Joi.string().max(100).required(),
+    username: Joi.string().min(6).max(50).required(),
+    email: Joi.string().required(),
+    age: Joi.number().integer().min(16).required(),
+    link: Joi.string(),
+  });
+
   endPoint = "https://jsonplaceholder.typicode.com/users";
 
   handleSubmit = async (e) => {
     e.preventDefault();
     const { data } = this.state;
     // validate data
+    const errors = this.validate();
+    this.setState({ errors: errors || {} });
+    if (errors) return;
+
     // submit data
     const { data: result } = await axios.post(this.endPoint, data);
     console.log("Response recived from https://jsonplaceholder.com", result);
+  };
+
+  validate = () => {
+    const { error } = this.schema.validate(this.state.data);
+    if (!error) return null;
+
+    const errors = {};
+    for (let i of error.details) errors[i.path[0]] = i.message;
+
+    return errors;
   };
 
   handleChange = ({ currentTarget: input }) => {
